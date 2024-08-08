1a78ab50a17137f492d9177b3f9df905ae5c2fee
sora-00 <sora.umiushi@gmail.com>
2024-08-08 11:39:58 +0900

    mapするdataファイルの作成

---
+++ app/components/home/NewsData.tsx
@@ -0,0 +1,11 @@
+export type NewsDataProps = {
+  date: string;
+  content: string;
+};
+
+export const NewsContentData: NewsDataProps[] = [
+  {
+    date: "2024.5.22",
+    content: "大会 すごい 優勝",
+  },
+];
