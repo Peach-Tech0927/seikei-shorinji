1fc24861778ba7a3ee3c3a6d4ee6550d77e1ae12
AruhaIwamoto <aruha0615@icloud.com>
2024-08-09 18:59:05 +0900

    不要なコードの削除とハンバーガーメニューの追加

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -1,89 +1,119 @@
 "use client";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
   Typography,
   Box,
-  useMediaQuery,
-  useTheme,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
 } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import Image from "next/image";
-import React from "react";
 import CustomLink from "./CustomLink";
+import "./style.css";
 
 const Header = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (open: boolean) => () => {
+    setOpen(open);
+  };
 
   return (
-    <AppBar
-      position="static"
-      color="inherit"
-      sx={{
-        bgcolor: "#a20000",
-        height: isMobile ? 70 : 85,
-      }}
-    >
-      <Toolbar
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          height: "100%",
-          textAlign: isMobile ? "center" : "left",
-          overflow: "hidden",
-        }}
-      >
-        <Box
-          display="flex"
-          alignItems="center"
-          gap={2}
-          py={isMobile ? 2 : 0}
-          justifyContent={isMobile ? "center" : "flex-start"}
-        >
-          <Image
-            src="/Shorinji（仮）.jpg"
-            alt="logo"
-            width={70}
-            height={70}
-            style={{
-              margin: isMobile ? 10 : 0,
-            }}
-          />
-          <Typography
-            color="white"
-            fontSize={isMobile ? 12 : 17}
-            lineHeight={1}
-            display="block"
-            textAlign="center"
+    <>
+      <AppBar position="static" color="inherit" className="appBar">
+        <Toolbar className="toolbar">
+          <Box className="boxLogo">
+            <Image
+              src="/Shorinji（仮）.jpg"
+              alt="logo"
+              width={70}
+              height={70}
+              className="image"
+            />
+            <Typography className="typography">
+              Seikei
+              <br /> Shorinji
+              <br /> Club
+            </Typography>
+          </Box>
+
+          <Box className="navLinks">
+            <CustomLink href="/home">HOME</CustomLink>
+            <CustomLink href="/about">ABOUT</CustomLink>
+            <CustomLink href="/member">MEMBER</CustomLink>
+            <CustomLink href="/match">MATCH</CustomLink>
+            <CustomLink href="/sns">SNS</CustomLink>
+            <CustomLink href="/news">NEWS</CustomLink>
+            <CustomLink href="/schedule">SCHEDULE</CustomLink>
+          </Box>
+
+          <IconButton
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer(true)}
+            className="menuButton"
+            sx={{ display: { xs: "flex", md: "none" } }}
           >
-            Seikei
-            <br /> Shorinji
-            <br /> Club
-          </Typography>
-        </Box>
-        <Box
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            gap: isMobile ? "8px" : "16px",
-            overflowX: isMobile ? "auto" : "hidden",
-            overflowY: "hidden",
-            whiteSpace: "nowrap",
-          }}
-        >
-          <CustomLink href="/home">HOME</CustomLink>
-          <CustomLink href="/about">ABOUT</CustomLink>
-          <CustomLink href="/member">MEMBER</CustomLink>
-          <CustomLink href="/match">MATCH</CustomLink>
-          <CustomLink href="/sns">SNS</CustomLink>
-          <CustomLink href="/news">NEWS</CustomLink>
-          <CustomLink href="/schedule">SCHEDULE</CustomLink>
-        </Box>
-      </Toolbar>
-    </AppBar>
+            <MenuIcon />
+          </IconButton>
+
+          <Drawer
+            anchor="right"
+            open={open}
+            onClose={toggleDrawer(false)}
+            sx={{ display: { xs: "block", md: "none" } }}
+          >
+            <Box
+              sx={{ width: 250 }}
+              role="presentation"
+              onClick={toggleDrawer(false)}
+              onKeyDown={toggleDrawer(false)}
+            >
+              <Box display="flex" justifyContent="flex-end" p={2}>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="close"
+                  onClick={toggleDrawer(false)}
+                >
+                  <CloseIcon />
+                </IconButton>
+              </Box>
+              <List>
+                <ListItem button component="a" href="/home">
+                  <ListItemText primary="HOME" />
+                </ListItem>
+                <ListItem button component="a" href="/about">
+                  <ListItemText primary="ABOUT" />
+                </ListItem>
+                <ListItem button component="a" href="/member">
+                  <ListItemText primary="MEMBER" />
+                </ListItem>
+                <ListItem button component="a" href="/match">
+                  <ListItemText primary="MATCH" />
+                </ListItem>
+                <ListItem button component="a" href="/sns">
+                  <ListItemText primary="SNS" />
+                </ListItem>
+                <ListItem button component="a" href="/news">
+                  <ListItemText primary="NEWS" />
+                </ListItem>
+                <ListItem button component="a" href="/schedule">
+                  <ListItemText primary="SCHEDULE" />
+                </ListItem>
+              </List>
+            </Box>
+          </Drawer>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
