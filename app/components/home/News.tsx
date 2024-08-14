a7dfc2f5ecc309fa00fa3f052cf3dbc86d9d20b2
sora-00 <sora.umiushi@gmail.com>
2024-08-14 15:24:10 +0900

    dividerのスタイルを調整

--- app/components/home/News.tsx
+++ app/components/home/News.tsx
@@ -8,21 +8,29 @@ import NewsCard from "./NewsCard";
 const News = () => {
   return (
     <>
-      <Box height={"80vh"} position={"relative"}>
-        <Heading title="NEWS" />
-      </Box>
-      <Box position={"relative"} marginTop={-78}>
-        {NewsData.map((data) => (
-          <Fragment key={data.date}>
-            <Box marginLeft={1.5}>
-              <NewsCard date={data.date} content={data.content} />
-            </Box>
-            <Divider
-              sx={{ width: "25%", marginTop: 2, marginBottom: 2 }}
-              color={"#f7f7f7"}
-            />
-          </Fragment>
-        ))}
+      <Box pl={45} py={28} height={"30vh"} justifyContent={"center"}>
+        <Box position={"relative"} pb={5}>
+          <Heading title="NEWS" />
+        </Box>
+        <Box position={"relative"}>
+          {NewsData.map((data) => (
+            <Fragment key={data.date}>
+              <Box marginLeft={1.2}>
+                <NewsCard date={data.date} content={data.content} />
+              </Box>
+              <Divider
+                sx={{
+                  width: "32%",
+                  marginTop: 2,
+                  marginBottom: 2,
+                  borderStyle: "dashed",
+                  borderWidth: 1,
+                  borderColor: "#cccccc",
+                }}
+              />
+            </Fragment>
+          ))}
+        </Box>
       </Box>
     </>
   );
