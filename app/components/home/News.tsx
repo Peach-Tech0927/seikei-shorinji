1f0db86008e17011d17aeb552f9a2c84b42a7f91
sora-00 <sora.umiushi@gmail.com>
2024-08-14 10:27:35 +0900

    Newsコンポーネントの雛形の作成

--- app/components/home/News.tsx
+++ app/components/home/News.tsx
@@ -1,10 +1,23 @@
-import { Box } from "@mui/material";
-import React from "react";
+import { Box, Divider } from "@mui/material";
+import React, { Fragment } from "react";
+
+import { NewsData } from "./NewsData";
+import Heading from "../common/Heading";
+import { relative } from "path";
 
 const News = () => {
   return (
     <>
-      <Box></Box>
+      <Box height={"80vh"}>
+        <Heading title="NEWS" />
+      </Box>
+      <Box>
+        {NewsData.map((data) => (
+          <Fragment key={data.date}>
+            <Divider sx={{ width: "100%" }} color={"#f7f7f7"} />
+          </Fragment>
+        ))}
+      </Box>
     </>
   );
 };
