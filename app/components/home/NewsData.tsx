ce971cb6dfbbb97ffc9104c5e12758e4fe97693e
sora-00 <sora.umiushi@gmail.com>
2024-08-14 14:57:15 +0900

    仮のデータを挿入

--- app/components/home/NewsData.tsx
+++ app/components/home/NewsData.tsx
@@ -8,4 +8,16 @@ export const NewsData: NewsDataProps[] = [
     date: "2024.5.22",
     content: "大会 すごい 優勝",
   },
+  {
+    date: "2024.5.22",
+    content: "大会 すごい 優勝",
+  },
+  {
+    date: "2024.5.22",
+    content: "大会 すごい 優勝",
+  },
+  {
+    date: "2024.5.22",
+    content: "大会 すごい 優勝",
+  },
 ];
