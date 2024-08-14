ec1b8347f1c1c97cd462804db5942b7903d43725
sora-00 <sora.umiushi@gmail.com>
2024-08-14 17:37:34 +0900

    Newsが二つ並んでいると仮定し、BoxタグをGridタグに変更してレスポンシブ対応

--- app/components/home/News.tsx
+++ app/components/home/News.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider } from "@mui/material";
+import { Box, Divider, Grid } from "@mui/material";
 import React, { Fragment } from "react";
 
 import { NewsData } from "./NewsData";
@@ -8,30 +8,62 @@ import NewsCard from "./NewsCard";
 const News = () => {
   return (
     <>
-      <Box pl={45} py={28} height={"30vh"} justifyContent={"center"}>
-        <Box position={"relative"} pb={5}>
-          <Heading title="NEWS" />
-        </Box>
-        <Box position={"relative"}>
-          {NewsData.map((data) => (
-            <Fragment key={data.date}>
-              <Box marginLeft={1.2}>
-                <NewsCard date={data.date} content={data.content} />
-              </Box>
-              <Divider
-                sx={{
-                  width: "32%",
-                  marginTop: 2,
-                  marginBottom: 2,
-                  borderStyle: "dashed",
-                  borderWidth: 1,
-                  borderColor: "#cccccc",
-                }}
-              />
-            </Fragment>
-          ))}
-        </Box>
-      </Box>
+      <Grid
+        container
+        pt={{ xs: 10, md: 20 }}
+        pb={{ xs: 50, md: 36 }}
+        justifyContent={"center"}
+        spacing={{ xs: 45, md: 5 }}
+      >
+        <Grid item xs={10} md={3.5} height={"30vh"}>
+          <Box position={"relative"} pb={{ xs: 3, md: 5 }}>
+            <Heading title="NEWS" />
+          </Box>
+          <Box position={"relative"}>
+            {NewsData.map((data) => (
+              <Fragment key={data.date}>
+                <Box marginLeft={1.2}>
+                  <NewsCard date={data.date} content={data.content} />
+                </Box>
+                <Divider
+                  sx={{
+                    width: "100%",
+                    marginTop: 2,
+                    marginBottom: 2,
+                    borderStyle: "dashed",
+                    borderWidth: 1,
+                    borderColor: "#cccccc",
+                  }}
+                />
+              </Fragment>
+            ))}
+          </Box>
+        </Grid>
+        <Grid item xs={10} md={3.5} height={"30vh"}>
+          <Box position={"relative"} pb={{ xs: 3, md: 5 }}>
+            <Heading title="NEWS" />
+          </Box>
+          <Box position={"relative"}>
+            {NewsData.map((data) => (
+              <Fragment key={data.date}>
+                <Box marginLeft={1.2}>
+                  <NewsCard date={data.date} content={data.content} />
+                </Box>
+                <Divider
+                  sx={{
+                    width: "100%",
+                    marginTop: 2,
+                    marginBottom: 2,
+                    borderStyle: "dashed",
+                    borderWidth: 1,
+                    borderColor: "#cccccc",
+                  }}
+                />
+              </Fragment>
+            ))}
+          </Box>
+        </Grid>
+      </Grid>
     </>
   );
 };
