187ddc7b048693384a5cd3ae071c7b16ea8338ed
sora-00 <sora.umiushi@gmail.com>
2024-08-14 17:50:54 +0900

    不要な記述の削除

--- app/components/common/Heading.tsx
+++ app/components/common/Heading.tsx
@@ -6,22 +6,11 @@ type Tprops = {
 
 const Heading: React.FC<Tprops> = ({ title }) => {
   return (
-    <Box
-      display={"flex"}
-      alignItems={"flex-start"}
-      justifyContent={"center"}
-      flexDirection={"column"}
-      width={"100%"}
-    >
-      <Typography
-        component="h3"
-        fontSize={{ xs: 18, md: 25 }}
-        pb={0.5}
-        fontWeight={"bold"}
-      >
+    <Box display={"flex"} alignItems={"flex-start"} flexDirection={"column"}>
+      <Typography fontSize={{ xs: 18, md: 25 }} pb={0.5} fontWeight={"bold"}>
         {title}
       </Typography>
-      <Box width="100%" height={1.5} bgcolor="#00154b" />
+      <Box width="100%" height={1.2} bgcolor="#00154b" />
     </Box>
   );
 };
