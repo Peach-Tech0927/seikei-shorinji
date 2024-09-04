ef3564f8be5a1df14fbf92295f36393cab1160a3
AruhaIwamoto <aruha0615@icloud.com>
2024-09-05 00:37:21 +0900

    Gridタグの変更と不要なコードの削除

--- app/components/common/Footer.tsx
+++ app/components/common/Footer.tsx
@@ -11,12 +11,6 @@ import {
 } from "@/app/data/FooterItemList";
 
 const Footer = () => {
-  const [open, setOpen] = useState(false);
-
-  const toggleDrawer = (open: boolean) => () => {
-    setOpen(open);
-  };
-
   return (
     <>
       <AppBar
@@ -24,26 +18,21 @@ const Footer = () => {
         color="inherit"
         sx={{
           backgroundColor: "#a20000",
-          py: 0,
-          display: "flex",
-          flexDirection: "column",
         }}
       >
         <Box
           py={"10px"}
           textAlign={"center"}
           mb={"20px"}
-          position="relative"
-          sx={{
-            backgroundColor: "#fff",
-            borderTop: "5px double #F58220",
-            borderBottom: "5px double #F58220",
-          }}
+          position={"relative"}
+          bgcolor={"#fff"}
+          borderTop={"5px double #F58220"}
+          borderBottom={"5px double #F58220"}
         >
           <Typography
-            variant="h6"
             color="#AD002D"
-            sx={{ fontWeight: "bold", fontSize: { xs: "16px", md: "20px" } }}
+            fontWeight={"bold"}
+            fontSize={{ xs: "16px", md: "20px" }}
           >
             CONTENTS
           </Typography>
@@ -60,79 +49,77 @@ const Footer = () => {
             backgroundColor: "#a20000",
           }}
         >
-          <Grid container spacing={5} sx={{ mb: { xs: 1, md: 0 } }}>
-            <Grid
-              item
-              xs={12}
-              md={3}
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-            >
+          <Box
+            display="flex"
+            flexDirection={{ xs: "column", md: "row" }}
+            justifyContent="space-between"
+            alignItems="flex-start"
+            mb={{ xs: 1, md: 0 }}
+          >
+            <Box display="flex" alignItems="center" marginX={10}>
               <Image
                 src="/Shorinji（仮）.jpg"
                 alt="logo"
                 width={100}
                 height={100}
               />
-
               <Typography
                 color="white"
                 fontSize={{ xs: "14px", md: "18px" }}
                 display="flex"
                 flexDirection="column"
                 alignItems="center"
-                sx={{ textAlign: "center", px: { xs: 1, sm: 2 }, py: 1 }}
+                px={{ xs: 1, sm: 2 }}
+                py={1}
+                fontWeight={"bold"}
               >
                 Seikei
                 <br /> Shorinji
                 <br /> Club
               </Typography>
-            </Grid>
+            </Box>
 
-            <Grid item xs={12} md={6}>
-              <Grid container spacing={4}>
-                <Grid item xs={6} sm={4}>
-                  <Box color="white">
-                    <Typography variant="h6" component="div" sx={{ mb: 1 }}>
-                      AAAAAAA
-                    </Typography>
-                    {FooterItemListA.map((Item) => (
-                      <Box key={Item.href} sx={{ mb: 1 }}>
-                        <CustomLink href={Item.href}>{Item.title}</CustomLink>
-                      </Box>
-                    ))}
-                  </Box>
-                </Grid>
+            <Grid container spacing={9}>
+              <Grid item xs={6} sm={4}>
+                <Box color="white">
+                  <Typography variant="h6" mb={1}>
+                    AAAAAAA
+                  </Typography>
+                  {FooterItemListA.map((Item) => (
+                    <Box key={Item.href} sx={{ mb: 1 }}>
+                      <CustomLink href={Item.href}>{Item.title}</CustomLink>
+                    </Box>
+                  ))}
+                </Box>
+              </Grid>
 
-                <Grid item xs={6} sm={4}>
-                  <Box color="white">
-                    <Typography variant="h6" component="div" sx={{ mb: 1 }}>
-                      BBBBBBBB
-                    </Typography>
-                    {FooterItemListB.map((Item) => (
-                      <Box key={Item.href} sx={{ mb: 1 }}>
-                        <CustomLink href={Item.href}>{Item.title}</CustomLink>
-                      </Box>
-                    ))}
-                  </Box>
-                </Grid>
+              <Grid item xs={6} sm={4}>
+                <Box color="white">
+                  <Typography variant="h6" mb={1}>
+                    BBBBBBBB
+                  </Typography>
+                  {FooterItemListB.map((Item) => (
+                    <Box key={Item.href} mb={1}>
+                      <CustomLink href={Item.href}>{Item.title}</CustomLink>
+                    </Box>
+                  ))}
+                </Box>
+              </Grid>
 
-                <Grid item xs={12} sm={4}>
-                  <Box color="white">
-                    <Typography variant="h6" component="div" sx={{ mb: 1 }}>
-                      CCCCCCCCCCC
-                    </Typography>
-                    {FooterItemListC.map((Item) => (
-                      <Box key={Item.href} sx={{ mb: 1 }}>
-                        <CustomLink href={Item.href}>{Item.title}</CustomLink>
-                      </Box>
-                    ))}
-                  </Box>
-                </Grid>
+              <Grid item xs={12} sm={4}>
+                <Box color="white">
+                  <Typography variant="h6" mb={1}>
+                    CCCCCCCCCCC
+                  </Typography>
+                  {FooterItemListC.map((Item) => (
+                    <Box key={Item.href} mb={1}>
+                      <CustomLink href={Item.href}>{Item.title}</CustomLink>
+                    </Box>
+                  ))}
+                </Box>
               </Grid>
             </Grid>
-          </Grid>
+          </Box>
         </Toolbar>
       </AppBar>
     </>
