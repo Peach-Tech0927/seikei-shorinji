e3d5d91cee90f6e914aa9ae44cf6d42783eccc3f
AruhaIwamoto <aruha0615@icloud.com>
2024-10-07 16:16:44 +0900

    itemsのmapを削除

--- app/components/common/FooterColumn.tsx
+++ app/components/common/FooterColumn.tsx
@@ -1,28 +1,15 @@
-import React from "react";
-import { Box, Typography, Grid } from "@mui/material";
-import CustomLink from "./CustomLink";
+import { Typography } from "@mui/material";
 
-interface FooterItem {
-  href: string;
+type FooterColumnProps = {
   title: string;
-}
+};
 
-interface FooterColumnProps {
-  title: string;
-  items: FooterItem[];
-}
-
-const FooterColumn: React.FC<FooterColumnProps> = ({ title, items }) => (
-  <Grid item xs={6} sm={4}>
+const FooterColumn: React.FC<FooterColumnProps> = ({ title }) => {
+  return (
     <Typography variant="h6" mb={1} color={"white"}>
       {title}
     </Typography>
-    {items.map((Item) => (
-      <Box key={Item.href} mb={1}>
-        <CustomLink href={Item.href}>{Item.title}</CustomLink>
-      </Box>
-    ))}
-  </Grid>
-);
+  );
+};
 
 export default FooterColumn;
