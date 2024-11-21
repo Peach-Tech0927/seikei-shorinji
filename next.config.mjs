d05fee6c4726bd9e17349f9ae535a481ef5ee0d6
AruhaIwamoto <aruha0615@icloud.com>
2024-11-21 11:00:46 +0900

    Imageを使用するためにコードを追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "user0514.cdnw.net",
+        pathname: "/shared/img/thumb/**",
+      },
+      {
+        protocol: "https",
+        hostname: "th.bing.com",
+        pathname: "/th/id/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
