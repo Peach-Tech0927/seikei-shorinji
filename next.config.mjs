9165f8f656e98526cf7965033c004d01b6865895
AruhaIwamoto <aruha0615@icloud.com>
2024-11-27 15:40:37 +0900

    不要なコードの削除

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "user0514.cdnw.net",
-        pathname: "/shared/img/thumb/**",
-      },
-      {
-        protocol: "https",
-        hostname: "th.bing.com",
-        pathname: "/th/id/**",
-      },
-    ],
-  },
-};
-
+const nextConfig = {};
 export default nextConfig;
